4f9df14484a3756e8b0e7a5d2646b88d49efbd66
Jim Yin <xiangpiduzi@gmail.com>
2015-06-07 02:30:02 +0800

    restore: init club id in common.js

--- src/main/resources/public/js/common.js
+++ src/main/resources/public/js/common.js
@@ -65,6 +65,16 @@ var forceNumber = function(editable) {
 })(jQuery);
 
 ;(function() {
+    // init user ID / club ID
+    $.get("/rest/whoami", function(userId) {
+        $('#userId').html('<i class="fa fa-user"></i> '+userId+' <span class="caret"></span>');
+        $('#clubId').text($.QueryString.clubId ? $.QueryString.clubId : userId); // for PJ page
+        $('body').attr('clubId', $.QueryString.clubId ? $.QueryString.clubId : userId); // for CA page
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("ERROR STATUS: "+textStatus);
+        showAlert("alert-danger", "Cannot find club info. Please refresh and retry.");
+    });
+
     $('#btnChangePassword').click(function() {
         $('#dlgPassword').modal({});
     });
